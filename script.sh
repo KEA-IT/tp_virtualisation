783a2b868280cd476f4310dd3ef92b5b0f32f709
Kevin AMARAL <k.amaral@it-students.fr>
2019-10-09 22:08:06 +0200

    Modification du fichier Vagrantfile
    le changement d'ip est opérationnel

--- script.sh
+++ script.sh
@@ -1,74 +1,95 @@
 #!/bin/bash
-if [[ ! -e Vagrantfile ]]
-then
+if [[ ! -e Vagrantfile ]]; then
     vagrant init
 else
     echo " Le fichier vagrantFile est déjà créé"
 fi
+#------déclaration variable     ------#
+ip="192.168.33.10"
+sync="data"
+box="ubuntu/xenial64"
 
 #------déclaration des fonctions------#
-menu(){
-clear
-echo "-----------------------------------------------------"
-echo "   Bienvenue dans l'éditeur de fichier vagrantFile   "
-echo "-----------------------------------------------------"
-echo " "
-echo " 1- Modifier l'adresse ip de la machine virtuel"
-echo " 2- Modifier le nom du dossier local de sycronisation"
-echo " 3- Modifier la box de la machine virtuel"   
-echo " 4- Modifier les paquet à installer"
-echo " 5- Reset le fichier vagrantFile"
-echo " 6- Quiter l'éditeur"
-read -p " Votre choix ? (1, 2, 3, 4) : " modify_choice
-case $modify_choice in
+menu() {
+    clear
+    echo "-----------------------------------------------------"
+    echo "   Bienvenue dans l'éditeur de fichier vagrantFile   "
+    echo "-----------------------------------------------------"
+    echo " "
+    echo " 1- Modifier l'adresse ip de la machine virtuel"
+    echo " 2- Modifier le nom du dossier local de sycronisation"
+    echo " 3- Modifier la box de la machine virtuel"
+    echo " 4- Modifier les paquet à installer"
+    echo " 5- Reset le fichier vagrantFile"
+    echo " 6- Quiter l'éditeur"
+    read -p " Votre choix ? (1, 2, 3, 4) : " modify_choice
+    case $modify_choice in
     1)
         clear
-        echo "vous avez choisie 1"
+        read -p "quelle est la nouvelle adresse ip : " ip
+        echo "# -*- mode: ruby -*-" > Vagrantfile
+        echo "# vi: set ft=ruby :" >> Vagrantfile
+        echo "Vagrant.configure("2") do |config|" >> Vagrantfile
+        echo "config.vm.box = \"$box\"" >> Vagrantfile
+        echo "config.vm.network \"private_network\", ip: \"$ip\"" >> Vagrantfile
+        echo "config.vm.synced_folder \"$sync\", \"/vagrant_data\"" >> Vagrantfile
+        echo "end" >> Vagrantfile
+        if [[ $(grep -o -i $ip Vagrantfile | wc -l) == 1 ]]
+        then
+            echo "l'ip a correctement été changé"
+        elif [[ $(grep -o -i $ip Vagrantfile | wc -l) == 0 ]]
+        then
+            echo "l'ip na pas été changé"
+        else
+            echo "erreur lors de la modification ip"
+        fi
         read -p "On continue ?"
-        main;;
+        main
+        ;;
     2)
         clear
         echo "vous avez choisie 2"
         read -p "On continue ?"
-        main;;
+        main
+        ;;
     3)
         clear
         echo "vous avez choisie 3"
         read -p "On continue ?"
-        main;;
+        main
+        ;;
     4)
         clear
         echo "vous avez choisie 4"
         read -p "On continue ?"
-        main;;
+        main
+        ;;
     5)
-        main;;
-esac
-    
+        main
+        ;;
+    esac
+
 }
 
-main(){
-clear
-echo "-----------------------------------------------------"
-echo "    Bienvenue dans le créateur de machine virtuel    "
-echo "-----------------------------------------------------"
-echo " "
-echo " Le fichier vargantFile a été créé ici : "
-pwd
-echo " "
-read -p " voulez vous faire des modifications dans le fichier vagrantFile ? (o/n) : " change_file
-if [[ $change_file == "o" ]]
-then
-    menu
-elif [[ $change_file == "n" ]]
-then
-    echo "on va créer le dossier sync et lancer la vm"
-else
-    echo "Erreur lors de la saisie" && sleep 2
-    main
-fi
+main() {
+    clear
+    echo "-----------------------------------------------------"
+    echo "    Bienvenue dans le créateur de machine virtuel    "
+    echo "-----------------------------------------------------"
+    echo " "
+    echo " Le fichier vargantFile a été créé ici : "
+    pwd
+    echo " "
+    read -p " voulez vous faire des modifications dans le fichier vagrantFile ? (o/n) : " change_file
+    if [[ $change_file == "o" ]]; then
+        menu
+    elif [[ $change_file == "n" ]]; then
+        echo "on va créer le dossier sync et lancer la vm"
+    else
+        echo "Erreur lors de la saisie" && sleep 2
+        main
+    fi
 }
 
 #------main------#
 main
-
